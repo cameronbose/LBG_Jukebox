83bc7e40cedb1a95758a7457abe3e7d32f5f9be4
Cameron Bose <cameron.bose@fwdview.com>
2023-02-08 11:34:45 +0000

    adding Jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,40 @@
+properties([parameters([string(defaultValue: '10.44.1.160', name: 'dxEngineProd', trim: true), string(defaultValue: '10.44.1.119', name: 'dxEngineNonProd', trim: true), choice(choices: ['6.0.14.0', '6.0.15.0', '6.0.16.0', '6.0.17.0'], name: 'dxVersion'), string(defaultValue: 'FN', name: 'dSourceName', trim: true), string(defaultValue: 'VFN_RCP', name: 'vdbProdName', trim: true), string(defaultValue: 'LBG Replication', name: 'replicationName', trim: true), string(defaultValue: 'Masked Master Replication Template', name: 'templateName', trim: true), string(defaultValue: 'Template_VDB', name: 'templateVDBName', trim: true)])])
+pipeline { 
+    agent any 
+    environment { 
+        PROD_ENGINE = credentials('fwdview-prod-virtualization-engine')
+        NON_PROD_ENGINE = credentials('fwdview-non-prod-virtualization-engine')
+    }
+    stages { 
+        stage('Git Checkout') {
+            steps {
+                //bat 'rmdir /s /q LBG_Toolchain'; 
+                bat 'git clone https://github.com/cameronbose/LBG_Toolchain.git';
+            }
+        }
+        
+        stage("Executing API commands") { 
+            steps {
+                bat "python getParameters.py ${params.dxEngineProd} ${params.dxEngineNonProd} ${params.dxVersion} ${params.dSourceName} ${params.vdbProdName}, ${params.replicationName} ${params.templateName} ${params.templateVDBName} ${PROD_ENGINE_USR} ${PROD_ENGINE_PSW} ${NON_PROD_ENGINE_USR} ${NON_PROD_ENGINE_PSW}";    
+            }
+        } 
+    }
+    post { 
+        always { 
+            echo "Jenkins job run has completed."; 
+        }
+        success { 
+            echo "Job ran successfully!"; 
+        } 
+        failure { 
+            echo "Failed - please look at the error logs."; 
+        } 
+        unstable { 
+            echo "Jenkins run was unstable, please check logs."; 
+        } 
+        changed { 
+            echo "Job is now successful!"; 
+            
+        }
+    }
+}
--- getParameters.py
+++ getParameters.py
@@ -53,6 +53,9 @@ def checkAction(action,engine):
             print(state)
             if state == "COMPLETED":
                 return True 
+            elif state =="FAILED":
+                state="FAILED"
+                return state
             else: 
                 return False
 
@@ -67,13 +70,14 @@ def checkActionLoop(action,engine):
         if checkAction(action,engine):
             print("It has Completed!") 
             break 
+        elif checkAction(action,engine) == "FAILED": 
+            print("Action has failed, check engine logs.")
+            sys.exit()
         else:
             print("Not yet Completed, check again in 10 seconds")
             time.sleep(10)
 if __name__ == "__main__": 
     
-    username = "admin"
-    password = "fwdview01!"
     dxEngineProd = sys.argv[1]
     dxEngineNonProd = sys.argv[2]
     dxVersion = sys.argv[3]
@@ -81,14 +85,20 @@ if __name__ == "__main__":
     vdbName = sys.argv[5]
     replicationName = sys.argv[6]
     templateName = sys.argv[7]
+    templateVDBName = sys.argv[8]
+    prod_username = sys.argv[9]
+    prod_password = sys.argv[10]
+    non_prod_username = sys.argv[11]
+    non_prod_password = sys.argv[12]
+
     major,minor,micro = getAPIVersion(dxVersion)
     
     print("Getting reference ID's for API POST calls. ")
-    os.system(f"sh login.sh 'admin' 'Fwdview01!' {dxEngineNonProd} {major} {minor} {micro}")
+    os.system(f"sh login.sh {non_prod_username} {non_prod_password} {dxEngineNonProd} {major} {minor} {micro}")
     templateReference,templateBranch = getTemplateBranch(templateName)
     
     print("logging in") 
-    os.system(f"sh login.sh {username} {password} {dxEngineProd} {major} {minor} {micro}")
+    os.system(f"sh login.sh {prod_username} {prod_password} {dxEngineProd} {major} {minor} {micro}")
     
     sourceID = getdSourceContainerID(dSourceName,dxEngineProd)
     vdbID = getdSourceContainerID(vdbName,dxEngineProd)
@@ -100,33 +110,37 @@ if __name__ == "__main__":
     action = getAction()
     checkActionLoop(action,dxEngineProd)
     
+    time.sleep(5)
+
     latestSnap = getSnapshotID(sourceID,dxEngineProd)
     print(latestSnap)
     
-    print("Refreshing masked vdb in PROD environment to latest Snapshot just taken & then replicating to Non-Prod Environment.")
+    print("Refreshing masked vdb in PROD environment to latest Snapshot.")
     os.system(f"sh refreshVDBProd.sh {dxEngineProd} {vdbID} {latestSnap}")
     
     action = getAction()
     checkActionLoop(action,dxEngineProd)
-
+    
+    print("Replicating to Non-Prod Environment.")
     os.system(f"sh replicate.sh {dxEngineProd} {specID}")
 
     action = getAction()
     checkActionLoop(action,dxEngineProd)
 
 
-    os.system(f"sh login.sh 'admin' 'Fwdview01!' {dxEngineNonProd} {major} {minor} {micro}")
-    templateID = getdSourceContainerID("Template_VDB",dxEngineNonProd)
+    os.system(f"sh login.sh {non_prod_username} {non_prod_password} {dxEngineNonProd} {major} {minor} {micro}")
+    templateID = getdSourceContainerID(templateVDBName,dxEngineNonProd)
     templateReference,templateBranch = getTemplateBranch(templateName)
     # templateLatestSnap = getSnapshotID(templateID,dxEngineNonProd)
     # # do we need the snapID? 
     # print(f"{templateID} && {templateLatestSnap}")
     # print("Refreshing template vdb & creating bookmark on template.")
+    print("Refreshing Non-Prod Template VDB.")
     os.system(f"sh refreshVDBNonProd.sh {dxEngineNonProd} {templateID}")
     
     action = getAction()
     checkActionLoop(action,dxEngineNonProd)
-
+    print("Creating bookmark on Template.")
     os.system(f"sh bookmark.sh {dxEngineNonProd} {templateReference} {templateBranch}")
     
     action = getAction()
